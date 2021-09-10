87a48cca19ce27f0c56909dc6da6fbc66174c5bc
annaschultze <59482746+annaschultze@users.noreply.github.com>
2021-09-10 17:33:51 +0100

    scale all risks by size of month

--- analysis/030_descriptive_mortality_rates.R
+++ analysis/030_descriptive_mortality_rates.R
@@ -34,6 +34,10 @@ format_table <- function(data, outcome) {
   {{data}} %>% 
     # create a labelled variable for outputting in table formats 
     mutate(care_home_group = ifelse((care_home_type == "Yes"), "Care_or_Nursing_Home", "Private_Home")) %>%
+    # scale mortality risks and confidence intervals 
+    mutate(PointEst = PointEst/(days_in_month(as.Date(date, "%Y-%m-%d")))*30, 
+           Lower = Lower/(days_in_month(as.Date(date, "%Y-%m-%d")))*30, 
+           Upper = Upper/(days_in_month(as.Date(date, "%Y-%m-%d")))*30) %>% 
     # rename key variables to how you want them displayed in tables and select the relevant ones
     mutate(Mortality_Rate = round((PointEst*1000),2), 
            Confidence_Interval = paste(round(Lower*1000,2), round(Upper*1000,2), sep = "-")) %>% 
@@ -69,7 +73,7 @@ plot_figure <- function(data, axistext) {
   ystring <- paste(as_label(enquo(axistext)), "Mortality Risk per 1,000 individuals")
   titlestring <- paste(as_label(enquo(axistext)), "Mortality Risk by Age, crude")
   
-  ggplot({{data}}, aes (x = as.Date(date, "%Y-%m-%d"), y = value*1000, colour = ageband_narrow, linetype = care_home_type, group = interaction(ageband_narrow, care_home_type))) + 
+  ggplot({{data}}, aes (x = as.Date(date, "%Y-%m-%d"), y = ((value/(days_in_month(as.Date(date, "%Y-%m-%d")))*30)*1000), colour = ageband_narrow, linetype = care_home_type, group = interaction(ageband_narrow, care_home_type))) + 
     geom_line(size = 1) + geom_point() + 
     geom_vline(xintercept = as.numeric(as.Date("2020-02-01", "%Y-%m-%d")), colour = "gray48", linetype = "longdash") + 
     annotate(x=as.Date("2020-02-01"),y=+Inf,label="Wave 1",vjust=1, size = 3, geom="label") +
@@ -98,6 +102,22 @@ measure_any_age <- fread("./output/measure_allcause_death_age.csv", data.table =
 measure_covid_age <- fread("./output/measure_covid_death_age.csv", data.table = FALSE, na.strings = "")
 measure_noncovid_age <- fread("./output/measure_noncovid_death_age.csv", data.table = FALSE, na.strings = "")
 
+# Set rows with < 5 events to NA ----------------------------------------
+## and scale monthly estimates 
+
+measure_any_age <- measure_any_age %>% 
+  mutate(value = ifelse(ons_any_death <= 5, NA, value), 
+         ons_any_death = ifelse(ons_any_death <= 5, NA, ons_any_death)) 
+
+
+measure_covid_age <- measure_covid_age %>% 
+  mutate(value = ifelse(ons_covid_death <= 5, NA, value), 
+         ons_covid_death = ifelse(ons_covid_death <= 5, NA, ons_covid_death)) 
+
+measure_noncovid_age <- measure_noncovid_age %>% 
+  mutate(value = ifelse(ons_noncovid_death <= 5, NA, value), 
+         ons_noncovid_death = ifelse(ons_noncovid_death <= 5, NA, ons_noncovid_death)) 
+
 # Confidence Intervals ----------------------------------------------------
 # calculate confidence intervals using binconf function from Hmisc 
 # bind output into the original dataset 
@@ -105,31 +125,6 @@ measure_noncovid_age <- fread("./output/measure_noncovid_death_age.csv", data.ta
 measure_any_age <- as_tibble(cbind(measure_any_age,((binconf(measure_any_age$ons_any_death, measure_any_age$registered_at_start, alpha = 0.05, method = "wilson")))))
 measure_covid_age <- as_tibble(cbind(measure_covid_age,((binconf(measure_covid_age$ons_covid_death, measure_covid_age$registered_at_start, alpha = 0.05, method = "wilson")))))
 measure_noncovid_age <- as_tibble(cbind(measure_noncovid_age,((binconf(measure_noncovid_age$ons_noncovid_death, measure_noncovid_age$registered_at_start, alpha = 0.05, method = "wilson")))))
-
-# Set rows with < 5 events to NA ----------------------------------------
-# removing events, percentage and CIs 
-
-measure_any_age <- measure_any_age %>% 
-  mutate(value = ifelse(ons_any_death <= 5, NA, value), 
-         ons_any_death = ifelse(ons_any_death <= 5, NA, ons_any_death), 
-         PointEst = ifelse(ons_any_death <= 5, NA, PointEst), 
-         Lower = ifelse(ons_any_death <= 5, NA, Lower), 
-         Upper = ifelse(ons_any_death <= 5, NA, Upper), 
-         ) 
-
-measure_covid_age <- measure_covid_age %>% 
-  mutate(value = ifelse(ons_covid_death <= 5, NA, value), 
-         ons_covid_death = ifelse(ons_covid_death <= 5, NA, ons_covid_death), 
-         PointEst = ifelse(ons_covid_death <= 5, NA, PointEst),
-         Lower = ifelse(ons_covid_death <= 5, NA, Lower),
-         Upper = ifelse(ons_covid_death <= 5, NA, Upper)) 
-
-measure_noncovid_age <- measure_noncovid_age %>% 
-  mutate(value = ifelse(ons_noncovid_death <= 5, NA, value), 
-         ons_noncovid_death = ifelse(ons_noncovid_death <= 5, NA, ons_noncovid_death), 
-         PointEst = ifelse(ons_noncovid_death <= 5, NA, PointEst),          
-         Lower = ifelse(ons_noncovid_death <= 5, NA, Lower),
-         Upper = ifelse(ons_noncovid_death <= 5, NA, Upper)) 
 
 # Tables ------------------------------------------------------------------
 
